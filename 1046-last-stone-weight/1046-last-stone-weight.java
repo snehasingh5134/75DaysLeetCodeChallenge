82b8888e53c710013b70aa278f46e8a9c2528720
SNEHA SINGH <singhsnehasingh610@gmail.com>
2026-05-23 10:39:00 +0530

    Time: 2 ms (36.01%), Space: 42.9 MB (32.65%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>((a,b) -> b-a);
+
+        for(int stone : stones){
+            pq.offer(stone);
+        }
+
+        while(pq.size() > 1){
+
+            int y = pq.poll(); // largest
+            int x = pq.poll(); // second largest
+
+            if(y != x){
+                pq.offer(y - x);
+            }
+        }
+
+        return pq.isEmpty() ? 0 : pq.peek();
+    }
+}
